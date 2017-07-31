7752589b441041392ca2a48f4753917770ad7fc3
Jacob Marshall <jacob@pageproof.com>
2017-08-01 10:56:03 +1200

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello World")
+	})
+	log.Println("Ello!")
+	log.Fatalln(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+}
